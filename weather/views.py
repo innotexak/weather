77e6e9ffe1f3e23a1c2152b8d65c063274b339ad
Collins Abadaike <akuhinnocent2016@gmail.com>
2021-03-25 18:01:25 +0100

    First commit

--- weather/views.py
+++ weather/views.py
@@ -3,25 +3,28 @@ from django.http import HttpResponse, JsonResponse
 from .forms import WeatherForm
 import requests
 import json
+import datetime
 from django.conf import settings 
 from requests.exceptions import HTTPError
 
 
 api_key = settings.API_KEY
 
-def home_view(request):
-    
-    return render(request, 'index.html', {})
 
 
 def weather_view(request):
-    # buffer = io.BytesIO()
     city = request.GET.get('city')
     url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}'
     details = []
-    response = requests.get(url)
-    json_response = response.json()      
-    response = {
+    try:
+        response = requests.get(url)
+    except:
+        print("Error occured!")
+        
+        
+    json_response = response.json() 
+    time = datetime.datetime.now()     
+    server_response = {
         'description':json_response['weather'][0]['description'],
         'cloud':json_response['clouds']['all'],
         'wind':json_response['wind']['speed'],
@@ -29,12 +32,11 @@ def weather_view(request):
         'max_temp':int(json_response['main']['temp_max'] - 273.15 ),
         'state':json_response['name'],
         'country':json_response['sys']['country'],
+        'timestamp': f'{time.strftime("%c")}'
         }
-    details.append(response)
-        
+    details.append(server_response)        
     form = WeatherForm()
-
-    return render(request, 'forecast.html', {
+    return render(request, 'index.html', {
                                             'detailed_data': details,
                                             'forms':form
                                             })
